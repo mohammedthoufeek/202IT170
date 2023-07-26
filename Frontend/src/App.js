6b43fcaa0100dfaf26e41d673ae33dc434d44de8
MOHAMMED THOUFEEK A <www.thoufeekabc@gmail.com>
2023-07-26 12:06:56 +0530

    final commit

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
 import UserCreate from './components/UserCreate';
-import showTrain from './components/showTrain'
+// import showTrain from './components/showTrain'
 import { BrowserRouter as Router, Routes, Route ,Link} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <h1 className='Header'>John Doe Train Reservation </h1>
-        <Router>
+        <Router className='Head'>
           <Link to='/'>UserCreate</Link>
           <Link to='/showTrain'>showTrain</Link>
           <Routes>
             <Route path='/' element={<UserCreate/>}/>
-            <Route path='/showTrain' element={<showTrain/>}/>
+            {/* <Route path='/showTrain' element={<showTrain/>}/> */}
           </Routes>
         </Router>
     </div>
--- Frontend/src/components/UserCreate.js
+++ Frontend/src/components/UserCreate.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import './UserCreate.css'
 
 const UserCreate = () => {
   return (
     <div  className='User'>
-      UserCreate</div>
+      <h3>Enter Your Company:</h3>
+      <input className='input' placeholder='Enter your Company' />
+      <h3>Enter Your Name:</h3>
+      <input className='input' placeholder='Enter your Name' />
+      <h3>Roll No:</h3>
+      <input className='input' type='number'/>
+      <h3>Email:</h3>
+      <input className='input' type='mail'/>
+      <h3>Access Code:</h3>
+      <input className='input' type='code'/>
+      
+    </div>
   )
 }
 
